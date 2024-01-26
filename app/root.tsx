4cc78e4ed9609df955f33e794b48479c4cc315a0
ameetmadan <ameetmadan@gmail.com>
2024-01-26 11:32:05 +0100

    full remix tutorial

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,57 @@
 import {
   Form,
   Links,
+  NavLink,
   LiveReload,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
+  json,
+  redirect,
+  useLoaderData,
+  useNavigation,
+  useSubmit
 } from "@remix-run/react";
+import { createEmptyContact, getContacts } from "./data";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+import { useEffect } from "react";
+
+import appStylesHref from "./app.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return json({ contacts, q });
+};
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+};
 
 export default function App() {
+  const { contacts, q } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+  const submit = useSubmit();
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has(
+      "q"
+    );
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
   return (
     <html lang="en">
       <head>
@@ -20,10 +64,17 @@ export default function App() {
         <div id="sidebar">
           <h1>Remix Contacts</h1>
           <div>
-            <Form id="search-form" role="search">
+            <Form id="search-form" role="search" onChange={(event) => {
+              const isFirstSearch = q === null;
+              submit(event.currentTarget, {
+                replace: !isFirstSearch,
+              });
+            }}>
               <input
                 id="q"
                 aria-label="Search contacts"
+                className={searching ? "loading" : ""}
+                defaultValue={q || ''}
                 placeholder="Search"
                 type="search"
                 name="q"
@@ -35,17 +86,39 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive
+                          ? "active"
+                          : isPending
+                            ? "pending"
+                            : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
+                      <li>
+                        <a href={`/contacts/${contact.id}`}>{contact.first} {contact.last}</a>
+                      </li>
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
         </div>
-
+        <div id="detail" className={
+          navigation.state === "loading" ? "loading" : ""
+        }>
+          <Outlet />
+        </div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
